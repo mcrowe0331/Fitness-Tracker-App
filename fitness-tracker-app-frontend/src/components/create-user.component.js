17e08868a6df1b4b3353c8321c4656ce3ee2cc51
Mary Elizabeth <maryelizabeth@Marys-MacBook-Pro.local>
2022-12-11 14:29:40 -0600

    import axios

--- fitness-tracker-app-frontend/src/components/create-user.component.js
+++ fitness-tracker-app-frontend/src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreateUser extends Component {
   constructor(props) {
@@ -21,6 +22,8 @@ export default class CreateUser extends Component {
       username: this.state.username,
     };
     console.log(newUser);
+    axios.post('http://localhost:5001/users/add', newUser)
+    .then(res => console.log(res.data));
     
     this.setState({
       username: ''
