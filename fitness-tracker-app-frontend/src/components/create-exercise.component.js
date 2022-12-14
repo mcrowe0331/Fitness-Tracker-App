07047c9593475c736fc476e5365ebf8c603a211d
Leonel Alanis <l.alanis1989@gmail.com>
2022-12-13 19:01:36 -0600

    Exercise list route

--- fitness-tracker-app-frontend/src/components/create-exercise.component.js
+++ fitness-tracker-app-frontend/src/components/create-exercise.component.js
@@ -71,7 +71,7 @@ export default class CreateExercise extends Component {
   axios.post('http://localhost:5001/exercises/add', exercise)
   .then(res => console.log(res.data));
 
-  // window.location = '/';
+  window.location = '/';
   }
 
   render() {
